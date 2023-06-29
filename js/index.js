4776b6e6fe929b92ebdcd88b301077edd1c28156
mobin <mbynatrzade@gmail.com>
2023-06-29 19:13:08 +0430

    add typeWriter and makeCounter effect

---
+++ js/index.js
@@ -0,0 +1,37 @@
+const $ = document;
+const landingTitle = $.querySelector('.landing__title');
+const landingCoursesCount = $.querySelector('#courses-count');
+const landingMinutesCount = $.querySelector('#minutes-counter');
+const landingUsersCount = $.querySelector('#users-counter');
+
+window.addEventListener('load', () => {
+    let landingText = "ما به هر قیمتی دوره آموزشی تولید نمی کنیم !";
+    let typeIndex = 0;
+
+    typeWriter(landingText, typeIndex);
+    makeCounter(40, landingCoursesCount);
+    makeCounter(13_430, landingMinutesCount);
+    makeCounter(2_105, landingUsersCount);
+})
+
+function typeWriter(text, index) {
+    if (index < text.length) {
+        landingTitle.innerHTML += text[index];
+        index++;
+    }
+    setTimeout(() => {
+        typeWriter(text, index);
+    }, 100)
+}
+
+function makeCounter(max, elem) {
+    let counter = 0;
+    const interval = setInterval(() => {
+        if (counter === max) {
+            clearInterval(interval);
+        }
+
+        elem.innerHTML = counter;
+        counter++;
+    }, 0.5);
+}
