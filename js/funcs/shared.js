5f534401ab8ac5eaffae40ddb13e8a87dc5e03b3
mobin <mbynatrzade@gmail.com>
2023-07-17 18:33:24 +0430

    add show all courses in courses page

--- js/funcs/shared.js
+++ js/funcs/shared.js
@@ -765,6 +765,56 @@ const submitComment = async () => {
     }
 }
 
+const showAllCoursesInCoursesPage = async () => {
+    const coursesWrapperElem = document.querySelector('#courses-wrapper');
+
+    const res = await fetch(`http://localhost:4000/v1/courses`);
+    const courses = await res.json();
+
+    courses.forEach(course => {
+        coursesWrapperElem.insertAdjacentHTML('beforeend', `
+        <div class="col-4">
+        <div class="course-box">
+            <a href="course.html?name=${course.shortName}">
+                <img src=http://localhost:4000/courses/covers/${course.cover} alt="course img"
+                    class="course-box__img" />
+            </a>
+            <div class="course-box__main">
+                <a href="course.html?name=${course.shortName}" class="course-box__title">${course.name}</a>
+                <div class="course-box__rating-teacher">
+                    <div class="course-box__teacher">
+                        <i class="fas fa-chalkboard-teacher course-box__teacher-icon"></i>
+                        <a href="#" class="course-box__teacher-link">${course.creator}</a>
+                    </div>
+                    <div class="course-box__rating">
+                        ${Array(5 - course.courseAverageScore).fill(0).map((score)=> 
+                            '<img src="images/svgs/star.svg" alt="rating" class="course-box__star">'
+                            ).join('')}
+                        ${Array(course.courseAverageScore).fill(0).map((score)=> 
+                            '<img src="images/svgs/star_fill.svg" alt="rating" class="course-box__star">'
+                            ).join('')}
+                    </div>
+                </div>
+                <div class="course-box__status">
+                    <div class="course-box__users">
+                        <i class="fas fa-users course-box__users-icon"></i>
+                        <span class="course-box__users-text">${course.registers}</span>
+                    </div>
+                    <span class="course-box__price">${course.price === 0 ? "رایگان" : course.price.toLocaleString()}</span>
+                </div>
+            </div>
+            <div class="course-box__footer">
+                <a href="#" class="course-box__footer-link">
+                    مشاهده اطلاعات
+                    <i class="fas fa-arrow-left course-box__footer-icon"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+        `)
+    })
+}
+
 
 export {
     showUserNameInNavbar,
@@ -783,5 +833,6 @@ export {
     submitContactUsMsg,
     createNewNewsLetter,
     globalSearch,
-    submitComment
+    submitComment,
+    showAllCoursesInCoursesPage
 };
