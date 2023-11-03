686539dc9b433b77ff214c81afd217d5b7b46b76
mobin <mbynatrzade@gmail.com>
2023-11-03 17:27:50 +0330

    show notifications by admin

--- js/panel/shared.js
+++ js/panel/shared.js
@@ -7,16 +7,42 @@ const $ = document;
 window.addEventListener('load', () => {
     const adminWelcomeNameElem = $.querySelector('#admin-welcome-name');
     const adminNameElem = $.querySelector('#admin-name');
-    getAdminInfos().then(admin => {
-        console.log(admin);
+    const notificationsIconElem = $.querySelector('#notifications-icon');
+    const notificationsBoxElem = $.querySelector('.home-notification-modal');
+    const NotificationModalListElem = $.querySelector('.home-notification-modal-list');
 
-        // Protect Cms Routes
+    getAdminInfos().then(admin => {
+
+        // Protect CMS Routes
         if (admin.role === 'ADMIN') {
             // Show Admin Name In Cms HomePage
             adminNameElem.innerHTML = admin.name;
             adminWelcomeNameElem.innerHTML = admin.name;
         } else {
             location.replace('../../login.html');
+        }
+
+        notificationsIconElem.addEventListener('mouseenter', () => {
+            notificationsBoxElem.classList.add('active-modal-notfication');
+        })
+        notificationsIconElem.addEventListener('mouseleave', () => {
+            notificationsBoxElem.classList.remove('active-modal-notfication');
+        })
+        notificationsBoxElem.addEventListener('mouseleave', () => {
+            notificationsBoxElem.classList.remove('active-modal-notfication');
+        })
+
+        if (admin.notifications.length) {
+            admin.notifications.forEach(notification => {
+                NotificationModalListElem.insertAdjacentHTML('beforeend', `
+                    <li class="home-notification-modal-item">
+                        <span class="home-notification-modal-text">${notification.msg}</span>
+                        <a>دیدم</a>
+                    </li>
+                `)
+            })
+        } else {
+
         }
     })
 })
