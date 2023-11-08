8d33a6d8b077f06ce8214629d57b693439e3dbe0
mobin <mbynatrzade@gmail.com>
2023-11-08 17:32:20 +0330

    add seen and remove notification functions

--- js/panel/func/notifications.js
+++ js/panel/func/notifications.js
@@ -7,12 +7,13 @@ const insertNotificationHtmlTemplate = (notifications) => {
     const NotificationModalListElem = document.querySelector('.home-notification-modal-list');
 
 
+    NotificationModalListElem.innerHTML = '';
     if (notifications.length) {
         notifications.forEach(notification => {
             NotificationModalListElem.insertAdjacentHTML('beforeend', `
                 <li class="home-notification-modal-item">
                     <span class="home-notification-modal-text">${notification.msg}</span>
-                    <a onclick="seenNotification('${notification._id}')">دیدم</a>
+                    <a onclick='seenNotification(${JSON.stringify(notifications)}, ${JSON.stringify(notification._id)})'>دیدم</a>
                 </li>
             `)
         })
@@ -24,15 +25,22 @@ const insertNotificationHtmlTemplate = (notifications) => {
     }
 }
 
-const seenNotification = async (notificationId) => {
+const seenNotification = async (notifications, notificationId) => {
     const res = await fetch(`http://localhost:4000/v1/notifications/see/${notificationId}`, {
         method: 'PUT',
         headers: {
             Authorization: `Bearer ${getToken()}`,
         }
     })
+    removeNotification(notifications, notificationId);
 
     const result = await res.json();
+}
+
+const removeNotification = (notifications, notificationId) => {
+    const filteredNotifications = notifications.filter(notification => notification._id !== notificationId);
+
+    insertNotificationHtmlTemplate(filteredNotifications);
 }
 
 export {
