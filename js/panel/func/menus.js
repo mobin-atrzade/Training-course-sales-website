5a997c036c55eced53dd011b19d06b96b3bcddf3
mobin <mbynatrzade@gmail.com>
2023-12-05 17:37:17 +0330

    show all menus and sunMenus in p-Admin

--- js/panel/func/menus.js
+++ js/panel/func/menus.js
@@ -1,8 +1,30 @@
+
+
 const getAndShowAllMenus = async () => {
     const menusWrapperElem = document.querySelector('.table tbody');
 
-    const res = await fetch(`http://localhost:4000/v1/menus`);
+    const res = await fetch(`http://localhost:4000/v1/menus/all`);
     const menus = await res.json();
+
+    menus.forEach((menu, index) => {
+        menusWrapperElem.insertAdjacentHTML('beforeend', `
+        <tr>
+            <td>${index + 1}</td>
+            <td>${menu.title}</td>
+            <td><a href="#">${menu.href}</a></td>
+            <td>
+                ${menu.parent ? menu.parent.title : " --- "}
+            </td>
+            <td>
+                <button type="button" class="btn btn-primary edit-btn">ویرایش</button>
+            </td>
+            <td>
+                <button type="button" class="btn btn-danger delete-btn">حذف</button>
+            </td>
+        </tr>
+        `)
+    })
+
     return menus;
 }
 
